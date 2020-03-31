f4c63907af308b03ab066d2ad019fb554343f9eb
Aurelien <aurelien.badre.dev@gmail.com>
2020-03-31 13:14:06 +0200

    contact list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/82.jpg"
-        name="Riley Stone"
-        online=""
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/men/11.jpg"
-        name="Fred Harris"
-        online='Online'
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/72.jpg"
-        name="Marlene Carroll"
-        online=""
-      />
+      <ContactList />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,6 +23,7 @@ const Contact = (props) => {
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    online: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired,
 };
+
 export default Contact;
