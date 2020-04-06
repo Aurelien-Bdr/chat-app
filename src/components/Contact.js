658973016811b85d80204aa1232ba8390d2d3ec1
Aurelien <aurelien.badre.dev@gmail.com>
2020-04-06 18:10:03 +0200

    adding state componnent

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,27 +3,39 @@ import './Contact.css';
 import PropTypes from 'prop-types';
 
 
-const Contact = (props) => {
-    return (
-        <div className="Contact">
-            <img className="avatar"
-                src={props.avatar}
-                alt={props.name} />
-            <div>
-                <p className="name">{props.name}</p>
-                <div className="status">
-                    <div className={props.online ? 'status-online' : 'status-offline'} />
-                    <p className="status-text">{props.online ? 'Online' : 'Offline'}</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            online: props.online,
+        }
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar"
+                    src={this.props.avatar}
+                    alt={this.props.name} />
+                <div>
+                    <p className="name">{this.props.name}</p>
+                    <div className="status"
+                        onClick={event => {
+                            const newStatut = !this.state.online
+                            this.setState({ online: newStatut })
+                        }}
+                    >
+                        <div className={this.state.online ? 'status-online' : 'status-offline'} />
+                        <p className="status-text">{this.state.online ? 'Online' : 'Offline'}</p>
+                    </div>
                 </div>
-            </div>
-        </div >
-    );
+            </div >
+        )
+    }
 }
 
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    online: PropTypes.bool.isRequired,
 };
 
 export default Contact;
